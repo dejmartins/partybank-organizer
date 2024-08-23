389c609c97fdad3310e86a372c6fe59da55f5498
Deji Martins <dejimartins99@gmail.com>
2024-08-23 12:46:47 +0100

    styled the terms and condition paragraph

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,4 +1,5 @@
 import styles from '../../ui/styles/auth.module.css';
+import Image from 'next/image';
 
 export default function Page() {
     return (
@@ -8,16 +9,20 @@ export default function Page() {
           <p className='text-[18px] font-[400] leading-[23.81px]'>Please sign in or sign up below</p>
         </div>
 
-        <form>
-          <label>Email Address</label>
-          <input className='w-full' placeholder="dej@email.com" />
+        <form className='text-left'>
+          <label className="text-[18px] font-[500]">Email</label>
+          <input className="w-full rounded-[10px] p-[10px] mt-[8px] mb-[30px]" placeholder="dej@email.com" />
 
-          <button>Continue with Email</button>
-          <button>Continue with Google</button>
+          <button className='w-full p-[10px] rounded-[10px] bg-[var(--pb-c-red)] text-[var(--pb-c-white)] text-[18px] font-bold'>Continue with Email</button>
+
+          <hr className='my-8 border-[1px]' />
+          <button className='flex items-center rounded-[10px] justify-center w-full p-[10px] bg-[var(--pb-c-white)] text-[18px] font-[500]'>
+            <Image src="/google-icon.png" alt='Google Icon' width={20} height={20} className='mr-4' />
+            Continue with Google</button>
         </form>
         
 
-        <p>By continuing, you agree to have read and accepted party bank terms and conditions</p>
+        <p className='text-[18px] font-[400] leading-[23.81px]'>By continuing, you agree to have read and accepted party bank <span className='text-[var(--pb-c-red)] font-[500] underline underline-offset-4 cursor-pointer'>terms and conditions</span></p>
       </div>
     );
   }

22868634b23768342a8a504e1ea7192a9f03117d
Deji Martins <dejimartins99@gmail.com>
2024-08-23 18:37:25 +0100

    changed h3 font for mobile view

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
     return (
       <div className="flex flex-col items-center justify-between text-center bg-[var(--pb-c-soft-grey)] h-full rounded-[40px] p-8 md:p-12 text-[#080D18]">
         <div>
-          <h3 className='text-[32px] font-[700] leading-[42.34px]'>Welcome to Partybank</h3>
+          <h3 className='text-[29px] md:text-[32px] font-[700] leading-[35.34px] md:leading-[42.34px]'>Welcome to Partybank</h3>
           <p className='text-[15px] leading-[20.81px] md:text-[18px] font-[400] leading-[23.81px] mb-6'>Please sign in or sign up below</p>
         </div>
 

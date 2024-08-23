59f6c8b5bad69b3573c343e5316dd9cbe5312830
Deji Martins <dejimartins99@gmail.com>
2024-08-23 05:04:03 +0100

    hero image added

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,14 +1,23 @@
 import TopNav from "../ui/auth/topnav";
+import Image from "next/image";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex-grow flex flex-col overflow-hidden">
+        <div className="flex flex-col min-h-[100vh] max-h-[100vh] bg-[var(--pb-c-soft-grey)] overflow-hidden">
             <TopNav />
-            <div className="flex overflow-auto p-6 md:px-5">
-                <div>
-                    <p>Image Here</p>
+            <div className="flex flex-grow p-[24px] bg-[var(--pb-c-white)] mt-10 overflow-hidden">
+                <div className="flex-shrink-0 mr-6 rounded-[40px] overflow-hidden">
+                    <Image
+                        src="/hero-image.png"
+                        width={957}
+                        height={729}
+                        alt="Jazz musicians performing"
+                        className="object-contain"
+                    />
                 </div>
-                {children}
+                <div className="flex-grow overflow-auto">
+                    {children}
+                </div>
             </div>
         </div>
     )

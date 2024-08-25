bada9c64e7307c9d8a3e0fc238d3e0c48101761a
Deji Martins <dejimartins99@gmail.com>
2024-08-25 14:46:55 +0100

    dashboard layout structure

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,14 @@
-export default async function Layout() {
-    return <p>Layout</p>
+import TopNav from "../ui/dashboard/topnav";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="flex flex-col min-h-[100vh] bg-[var(--pb-c-soft-grey)] overflow-x-hidden">
+            <TopNav />
+            <div className="flex flex-col lg:flex-row flex-grow p-6 lg:p-8 bg-[var(--pb-c-white)] mt-10">
+                <div className="flex-grow w-full">
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
 }

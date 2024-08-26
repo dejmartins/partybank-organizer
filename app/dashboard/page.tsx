d57f3c326553b74bd342c78579fdace514b35255
Deji Martins <dejimartins99@gmail.com>
2024-08-26 02:29:20 +0100

    empty-state for dashboard web-view

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image"
+
 export default async function Page() {
     return (
         <div className="">
@@ -5,8 +7,14 @@ export default async function Page() {
                 <p className="text-[23px] md:text-[30px]">Welcome aboard, <span className="font-[700]">Michael!</span></p>
             </div>
 
-            <div className="p-6 lg:p-8">
-                <p>Body Here</p>
+            <div className="md:mt-20 p-6 lg:p-8">
+                <div className="w-full flex flex-col items-center absolute bottom-[12%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+                    <div className="bg-[#F8F9F9] rounded-[50px] w-[200px] h-[200px] flex items-center justify-center mb-3">
+                        <Image src='/empty-state.png' width={130} height={130} alt="Blank Phone Screens" />
+                    </div>
+                    <p className="font-[700]">No records yet!</p>
+                    <p className="text-[12px] md:text-[16px]">But we got you, when you start creating events</p>
+                </div>
             </div>
         </div>
     )

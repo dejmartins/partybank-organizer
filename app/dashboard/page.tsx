f6b2230ccc1739a67cb09bcfaf77528a9059403c
Deji Martins <dejimartins99@gmail.com>
2024-08-26 01:55:29 +0100

    changed title text-size for smaller screen

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@ export default async function Page() {
     return (
         <div className="">
             <div className="py-3 px-6 border-0 border-b-[3px] border-[var(--pb-c-soft-grey)]">
-                <p className="text-[30px]">Welcome aboard, <span className="font-[700]">Michael!</span></p>
+                <p className="text-[23px] md:text-[30px]">Welcome aboard, <span className="font-[700]">Michael!</span></p>
             </div>
 
             <div className="p-6 lg:p-8">

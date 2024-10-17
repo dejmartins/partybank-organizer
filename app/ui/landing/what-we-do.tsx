d74c9f118d109beb2eec3f1b0db1d1c12c09b891
Deji Martins <dejimartins99@gmail.com>
2024-10-17 21:11:24 +0100

    landing page fixed

--- app/ui/landing/what-we-do.tsx
+++ app/ui/landing/what-we-do.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function WhatWeDo() {
     return (
       <section className="py-16 bg-[#FFFFFF] my-5 rounded-[20px]">
-        <div className="container mx-auto px-3 md:px-6">
+        <div className="mx-auto px-3 md:px-6">
             <div className="flex justify-center">
                 <p className="border inline-block border border-[#FBD1D9] rounded-[20px] bg-[#FBD1D933] mb-10 p-2 px-3">What we do?</p>
             </div>
@@ -15,25 +15,52 @@ export default function WhatWeDo() {
           <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
 
             <div className="flex flex-col p-4">
-              <img src="/event-creation.svg" alt="Event Creation Simplified" className="w-full h-80 object-cover rounded-[20px] mb-4 border" />
+              <div className="relative border bg-[#FBFBFB] pb-0 mb-4 overflow-hidden h-44 lg:h-56 xl:h-80 rounded-[20px]">
+                <img 
+                  src="/event-creation.svg" 
+                  alt="Event Creation Simplified" 
+                  className="absolute -bottom-10 -right-10 h-full object-contain" 
+                />
+              </div>
+
               <h3 className="text-xl font-[700]">Event Creation Simplified</h3>
               <p className="text-gray-600">With Partybank, creating and managing your event is fast and straightforward.</p>
             </div>
-  
+
             <div className="flex flex-col p-4">
-              <img src="/real-time.svg" alt="Real-Time Event Monitoring" className="w-full h-80 object-cover rounded-[20px] mb-4 border" />
+              <div className="relative border bg-[#FBFBFB] pb-0 mb-4 overflow-hidden h-44 lg:h-56 xl:h-80 rounded-[20px]">
+                <img 
+                  src="/real-time.svg" 
+                  alt="Real-Time Event Monitoring" 
+                  className="absolute -bottom-10 -right-10 h-full object-contain" 
+                />
+              </div>
+
               <h3 className="text-xl font-[700]">Real-Time Event Monitoring</h3>
               <p className="text-gray-600">Track ticket sales, attendee engagement, and performance analytics in real time to make informed decisions during and leading up to your event.</p>
             </div>
-  
-            <div className="flex flex-col p-4 relative">
-              <img src="/smooth-ticket.svg" alt="Smooth Ticket Validation" className="w-full h-80 object-cover rounded-[20px] mb-4 border" />
+
+            <div className="flex flex-col p-4">
+              <div className="relative border bg-[#FBFBFB] pb-0 mb-4 p-10 overflow-hidden h-44 lg:h-56 xl:h-80 rounded-[20px]">
+                <img 
+                  src="/smooth-ticket.svg" 
+                  alt="Smooth Ticket Validation" 
+                  className="absolute bottom-0 left-1/2 transform -translate-x-1/2 h-auto w-[80%] object-contain" 
+                />
+              </div>
+
               <h3 className="text-xl font-[700]">Smooth Ticket Validation</h3>
               <p className="text-gray-600">Use our web app to scan and validate tickets at your event, ensuring quick entry for all attendees.</p>
             </div>
 
             <div className="flex flex-col p-4">
-              <img src="/flexible-payout.svg" alt="Flexible Payout Options" className="w-full h-80 object-cover rounded-[20px] mb-4 border" />
+              <div className="relative border bg-[#FBFBFB] p-2 mb-4 overflow-hidden h-44 lg:h-56 xl:h-80 rounded-[20px] flex items-center justify-center">
+                <img 
+                  src="/flexible-payout.svg" 
+                  alt="Flexible Payout Options" 
+                  className="h-auto w-[100%] object-contain" 
+                />
+              </div>
               <h3 className="text-xl font-[700]">Flexible Payout Options</h3>
               <p className="text-gray-600">Request payouts at any time, or receive automatic payments shortly after your event is complete.</p>
             </div>

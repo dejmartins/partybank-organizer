4791a088972267a33a240ad700fa924abb08f575
Deji Martins <dejimartins99@gmail.com>
2024-10-18 01:59:49 +0100

    privacy policy done

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -530,3 +530,46 @@ export const ticketCategoryData = [
     id: 2,
   },
 ];
+
+export const faqList = [
+  {
+      question: "What is Partybank and how does it work?",
+      answer:
+        "Partybank is an event management platform that allows you to create, manage, and sell tickets for your events. It helps both organizers and attendees by offering a seamless experience.",
+  },
+  {
+    question: "Is there customer support available?",
+    answer:
+      "Yes, Partybank offers dedicated customer support to assist event organizers. Whether you need help with setting up your event, tracking sales, or handling attendee issues, our support team is available to provide assistance.",
+  },
+  {
+    question: "Are there any fees for using Partybank?",
+    answer:
+      "Partybank charges a small service fee on each ticket sold. This fee can either be passed on to the attendee or absorbed by the event organizer, depending on your preference.",
+  },
+  {
+    question: "How do I get paid for ticket sales?",
+    answer:
+      "Event organizers can request a payout at any time before the event. However, if no request is made, organizers will automatically receive their payments after the event date, typically within a few hours or less than 24 hours after the event has concluded.",
+  },
+  {
+    question: "Is there a way to validate tickets at the event?",
+    answer:
+      "Yes, Partybank provides QR codes for each ticket. You can use our web app to scan and validate tickets at the event, ensuring a smooth check-in process.",
+  },
+  {
+    question: "Can I collect attendee data?",
+    answer:
+      "Currently, Partybank provides access to basic attendee information such as their email address and phone number. While the data collection is not fully customizable yet, these details are available to help you manage your attendees effectively.",
+  },
+  {
+    question: "Can I track ticket sales and attendee information?",
+    answer:
+      "Yes, Partybank offers real-time analytics that help you monitor ticket sales and view attendee details. This allows you to track event performance and gain insights into your audience.",
+  },
+  {
+    question: "How do I manage ticket types and pricing?",
+    answer:
+      "Partybank enables you to create multiple ticket types, such as General Admission or VIP. You can set different prices, create early-bird discounts, and control the availability of each ticket type through our intuitive event management tools.",
+  },
+];

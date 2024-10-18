1e4ac13c3fbf07dcbe8a5985dfe7e5631460bc9f
Deji Martins <dejimartins99@gmail.com>
2024-10-18 11:50:33 +0100

    linked logo to home screen

--- app/ui/dashboard/header.tsx
+++ app/ui/dashboard/header.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image"
 import { BellIcon, ChevronDownIcon, MagnifyingGlassIcon, UserCircleIcon } from '@heroicons/react/24/solid'
+import Link from "next/link"
 
 export default function Header() {
     return (
         <div className="flex items-center justify-between fixed top-0 left-0 right-0 bg-[var(--pb-c-white)] h-[64px] w-full px-[24px] py-[16px] z-50 border-0 border-b-4 border-[var(--pb-c-soft-grey)]">
             <div>
-                <Image
-                    src="/logo.svg"
-                    width={65.95}
-                    height={32.36}
-                    alt="Partybank Logo"
-                />
+                <Link href="/">
+                    <Image
+                        src="/logo.svg"
+                        width={65.95}
+                        height={32.36}
+                        alt="Partybank Logo"
+                    />
+                </Link>
             </div>
 
             <div className="flex gap-4">

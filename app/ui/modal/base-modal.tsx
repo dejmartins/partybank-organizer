5a055d5a32cc91ddbd4da5a12eb18de6a1f2a55e
Olabode <etoolabode@gmail.com>
2024-11-05 05:10:12 +0100

    ..event details modal on mobile

--- app/ui/modal/base-modal.tsx
+++ app/ui/modal/base-modal.tsx
@@ -11,7 +11,7 @@ export default function Modal({ isOpen, onClose, children }: ModalProps) {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 px-2">
       <div className="relative w-full max-w-md md:max-w-[700px] bg-white rounded-[30px] shadow-md">
         <div>{children}</div>
       </div>

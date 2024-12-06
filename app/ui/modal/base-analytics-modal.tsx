aea559fb7d2b0eacbcecc754a1718e65a69c999f
Abdulwarith <adebayoayomide646@gmail.com>
2024-12-06 16:47:55 +0100

    fix: fixed drawer responsive issue when opening analytics drawer

--- app/ui/modal/base-analytics-modal.tsx
+++ app/ui/modal/base-analytics-modal.tsx
@@ -16,7 +16,7 @@ export default function AnalyticsModal({
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="w-full h-[100vh] overflow-hidden">
+      <div className="w-full h-[90vh]">
         <div className="relative w-full m-auto animate-from-bottom max-w-md md:max-w-[90%] bg-white rounded-t-[30px] shadow-md ">
           <div>{children}</div>
         </div>
